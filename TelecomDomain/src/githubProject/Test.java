5a30f85c2114656aaef3ce41487b61dd2e606032
Ganeshkuklare <gskuklare123@gmail.com>
2022-07-07 17:28:04 +0530

    Test commit
    I have Completed My Work

---
+++ TelecomDomain/src/githubProject/Test.java
@@ -0,0 +1,12 @@
+package githubProject;
+
+public class Test {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("Ganesh Kukare");
+		System.out.println("SambhajiNagar");
+
+	}
+
+}
